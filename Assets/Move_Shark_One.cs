dc4b7051f8b38c71194556ee0ca232a512f27d86
kuarisu <kuarisuran@gmail.com>
2015-12-22 12:11:37 +0100

    ité move shark

---
+++ Assets/Move_Shark_One.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move_Shark_One : MonoBehaviour {
+
+
+    public GameObject RightForce;
+    public GameObject LeftForce;
+    private int speedForce = 275;
+    private float coolDown = 1;
+    private bool moveOKleft = true;
+    private bool moveOKright = true;
+
+
+    void Update()
+    {
+        if (Input.GetKeyUp("l") && moveOKleft)
+        {
+            StartCoroutine(LeftMovement());
+        }
+        if (Input.GetKeyUp("m") && moveOKright)
+        {
+            StartCoroutine(RightMovement());
+        }
+
+    }
+
+    //Coroutine ajout force à gauche du Player_One
+    IEnumerator LeftMovement()
+    {
+        moveOKleft = false;
+        GetComponent<Rigidbody>().AddForceAtPosition(Vector3.up * speedForce, LeftForce.transform.position);
+        yield return new WaitForSeconds(coolDown);
+        moveOKleft = true;
+
+    }
+
+    //Coroutine ajout force à droite du PLayer_One
+    IEnumerator RightMovement()
+    {
+        moveOKright = false;
+        GetComponent<Rigidbody>().AddForceAtPosition(Vector3.up * speedForce, RightForce.transform.position);
+        yield return new WaitForSeconds(coolDown);
+        moveOKright = true;
+    }
+}
