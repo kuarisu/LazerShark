90c9824a66fd24d671772e012512b5ebe0986e0a
kuarisu <kuarisuran@gmail.com>
2015-12-23 00:12:49 +0100

    ité 2

---
+++ Assets/LazerShot.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LazerShot : MonoBehaviour {
+
+    public GameObject lazer;
+    private bool shooting = false;
+
+    void OnTriggerExit(Collider col)
+    {
+        if ( col.gameObject.tag == "Water" && shooting == false)
+        {
+            shooting = true;
+            StartCoroutine(ShootLazer());
+        }
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Water")
+        {
+            shooting = false;
+            StopCoroutine("ShootLazer");
+        }
+    }
+
+    IEnumerator ShootLazer()
+    {
+        while (shooting == true)
+        {
+            Instantiate(lazer, transform.GetChild(0).transform.position, Quaternion.identity);
+            yield return new WaitForSeconds(5);
+        }
+
+    }
+}
