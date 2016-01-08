1f714013d0a1fece74f921a47960f071d04aa50e
kuarisu <kuarisuran@gmail.com>
2016-01-08 10:45:49 +0100

    ite

--- Assets/LazerMovement.cs
+++ Assets/LazerMovement.cs
@@ -10,7 +10,7 @@ public class LazerMovement : MonoBehaviour {
 	
     void Update ()
     {
-        transform.localPosition = transform.localPosition - transform.right * 0.2f;
+        transform.localPosition = transform.localPosition - transform.right * 0.3f;
     }
 
 
